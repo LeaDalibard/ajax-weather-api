830af62b8ab66814d4d2be50def3e18d648de9b7
Lea Dalibard <leadalibard@gmail.com>
2020-09-16 12:28:22 +0200

    Adding metrics to API to get celsius

--- script.js
+++ script.js
@@ -5,9 +5,9 @@
     function getData(){
         var city = document.getElementById("city").value;
         let weather;
-        axios.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+secretkey)
+        axios.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&appid="+secretkey)
             .then(result => {
-                weather=result.data
+                weather=result.data.list[1].dt
                 console.log(weather)
             }, error => {
                 alert("Type the name of a city.");
