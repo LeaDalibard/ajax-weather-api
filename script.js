9fb978ad4a00d5827868daa3b053dd57d145208f
Lea Dalibard <leadalibard@gmail.com>
2020-09-16 15:25:29 +0200

    Making a function with a loop to get an array with all the time lapses, every 3 hours =>response.data[list][i][dt_txt]

--- script.js
+++ script.js
@@ -11,12 +11,33 @@
                         alert("Type the name of a city.");
                     });
             }
-        )}
+        )
+    }
+
+    /*function getTime(response){
+         let timesInterval=[];
+         for (let i=0; i<)
+     }*/
+
+    function getTimeLapses(response) {
+        let timeLapses = [];
+        for (let i = 0; i < response.data["list"].length; i++) {
+            timeLapses.push(response.data["list"][i]["dt_txt"])
+        }
+        console.log(timeLapses);
+    }
+
 
     function getWeather() {
         var city = document.getElementById("city").value;
-        getData(city).then(response=>{
-            console.log(response)})
+        getData(city)
+            .then(response => {
+                //console.log(response.data.list[0].dt)})
+                //console.log(response.data["list"][0]["main"])})
+                //console.log(response.data["list"][0]["dt_txt"])})
+                getTimeLapses(response)
+            })
+
     }
 
 
