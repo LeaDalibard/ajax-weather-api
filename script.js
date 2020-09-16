ae85426dfcff15886c4a0de4a6c7cc9734ac0245
Lea Dalibard <leadalibard@gmail.com>
2020-09-16 12:05:13 +0200

    Pushing script.js + secret key

---
+++ script.js
@@ -0,0 +1,16 @@
+(() => {
+    var secretkey = config.SECRET_KEY;
+    run.addEventListener("click", getData)
+
+    function getData(){
+        var city = document.getElementById("city").value;
+        let weather;
+        axios.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+secretkey)
+            .then(result => {
+                weather=result.data
+                console.log(weather)
+            }, error => {
+                alert("Type the name of a city.");
+            });
+
+    }
