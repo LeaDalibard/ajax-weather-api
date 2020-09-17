2c4f4d2e1484a4737bf5fc81adee82e69b5bdaea
Lea Dalibard <leadalibard@gmail.com>
2020-09-17 10:18:02 +0200

    Trying to reorganise order of my promises, to dispatch data per day, value of the day not avalaible anymore in next promises

--- script.js
+++ script.js
@@ -14,110 +14,135 @@
         )
     }
 
-    /*function getTime(response){
-         let timesInterval=[];
-         for (let i=0; i<)
-     }*/
+    //----------------------Declaring variable
+
+
+    let feels_like = [];
+    let temp_min = [];
+    let temp_max = [];
+    let weather = [];
+
+    //---------------------- get TimeLapses and distributed them in 5 days
 
     function getTimeLapses(response) {
-        let timeLapses = [];
-        let day1 = [];
-        let day2 = [];
-        let day3 = [];
-        let day4 = [];
-        let day5 = [];
-        var dayFrom =new Array(5);
-       dayFrom[0] = new Date(response.data["list"][0]["dt_txt"]).getDay();
-        const numberDays=7;
-        for (let j=0;j<numberDays; j++){
-            if (dayFrom[j]<numberDays-1){dayFrom[j+1]=dayFrom[j]+1}
-            else (dayFrom[j+1]=0)
-                }
-        for (let i = 0; i < response.data["list"].length; i++) {
-            timeLapses.push(response.data["list"][i]["dt_txt"])
-        }
-        for (let i = 0; i < timeLapses.length; i++) {
-            if (new Date(response.data["list"][i]["dt_txt"]).getDay() == dayFrom[0]) {
-                day1.push(response.data["list"][i]["dt_txt"])
-            } else if (new Date(response.data["list"][i]["dt_txt"]).getDay() ==  dayFrom[1]) {
-                day2.push(response.data["list"][i]["dt_txt"])
-            } else if (new Date(response.data["list"][i]["dt_txt"]).getDay() ==  dayFrom[2]) {
-                day3.push(response.data["list"][i]["dt_txt"])
-            } else if (new Date(response.data["list"][i]["dt_txt"]).getDay() ==  dayFrom[3]) {
-                day4.push(response.data["list"][i]["dt_txt"])
-            }else  {
-                day5.push(response.data["list"][i]["dt_txt"])
+        return new Promise (resolve => {
+            let timeLapses = [];
+            let day1 = [];
+            let day2 = [];
+            let day3 = [];
+            let day4 = [];
+            let day5 = [];
+            var dayFrom = new Array(5);
+            dayFrom[0] = new Date(response.data["list"][0]["dt_txt"]).getDay();
+            const numberDays = 7;
+            for (let j = 0; j < numberDays; j++) {
+                if (dayFrom[j] < numberDays - 1) {
+                    dayFrom[j + 1] = dayFrom[j] + 1
+                } else (dayFrom[j + 1] = 0)
             }
+            for (let i = 0; i < response.data["list"].length; i++) {
+                timeLapses.push(response.data["list"][i]["dt_txt"])
+            }
+            for (let i = 0; i < timeLapses.length; i++) {
+                if (new Date(response.data["list"][i]["dt_txt"]).getDay() == dayFrom[0]) {
+                    day1.push(response.data["list"][i]["dt_txt"])
+                } else if (new Date(response.data["list"][i]["dt_txt"]).getDay() == dayFrom[1]) {
+                    day2.push(response.data["list"][i]["dt_txt"])
+                } else if (new Date(response.data["list"][i]["dt_txt"]).getDay() == dayFrom[2]) {
+                    day3.push(response.data["list"][i]["dt_txt"])
+                } else if (new Date(response.data["list"][i]["dt_txt"]).getDay() == dayFrom[3]) {
+                    day4.push(response.data["list"][i]["dt_txt"])
+                } else {
+                    day5.push(response.data["list"][i]["dt_txt"])
+                }
 
-        }
-        console.log(timeLapses);
-        console.log(day1);
-        console.log(day2);
-        console.log(day3);
-        console.log(day4);
-        console.log(day5);
-        console.log(dayFrom);
+            }
+            console.log(timeLapses);
+            console.log(day1);
+            console.log(day2);
+            console.log(day3);
+            console.log(day4);
+            console.log(day5);
+
+        })
+
     }
+    //---------------------- Distributed data per day
+
+    function distribute(array){
+        array.day1=array.splice(0, dayFrom.length-day1.length)
+            console.log(array.day1)
+    }
+
+
+
+
+    //---------------------- get Temperature global
 
     function getTemp(response) {
-        let temp = [];
+        return new Promise(resolve => {
+            let temp = [];
+            for (let i = 0; i < response.data["list"].length; i++) {
+                temp.push(response.data["list"][i]["main"]["temp"])
+            }
+            console.log(temp);
+        })
 
-        for (let i = 0; i < response.data["list"].length; i++) {
-            temp.push(response.data["list"][i]["main"]["temp"])
-        }
-
-        console.log(temp);
     }
 
+    //---------------------- get Feels like
 
     function getFeels_like(response) {
-        let feels_like = [];
         for (let i = 0; i < response.data["list"].length; i++) {
             feels_like.push(response.data["list"][i]["main"]["feels_like"])
         }
         console.log(feels_like);
     }
 
+    //---------------------- get temperature minimal
+
     function getTemp_min(response) {
-        let temp_min = [];
+
         for (let i = 0; i < response.data["list"].length; i++) {
             temp_min.push(response.data["list"][i]["main"]["temp_min"])
         }
         console.log(temp_min);
     }
 
+    //---------------------- get temperature maximal
+
     function getTemp_max(response) {
-        let temp_max = [];
+
         for (let i = 0; i < response.data["list"].length; i++) {
             temp_max.push(response.data["list"][i]["main"]["temp_max"])
         }
         console.log(temp_max);
     }
 
+//---------------------- get weather
 
     function getWeather(response) {
-        let weather = [];
         for (let i = 0; i < response.data["list"].length; i++) {
             weather.push(response.data["list"][i]["weather"][0]["main"])
         }
         console.log(weather);
     }
 
+//---------------------- get final results
+
     function doTheThing() {
         var city = document.getElementById("city").value;
         getData(city)
             .then(response => {
-                //console.log(response.data.list[0].dt)})
-                //console.log(response.data["list"][0]["main"])})
-                //console.log(response.data["list"][0]["dt_txt"])})
                 getTimeLapses(response)
-                getTemp(response)
+                temp=getTemp(response)
                 getFeels_like(response)
                 getTemp_min(response)
                 getTemp_max(response)
                 getWeather(response)
                 console.log(response.data["list"])
             })
+            .then(response=>{distribute(temp)})
 
     }
 
